af4af7227499f701142948d13d0bdf82de8002c9
eu3ler <eu3ler@achernar.uberspace.de>
2015-06-26 22:02:05 +0200

    Bugfix: Don't following symlinks

--- src/cfileselector.cpp
+++ src/cfileselector.cpp
@@ -24,13 +24,20 @@ void CFileSelector::process_directory(const boost::filesystem::path &path) {
 
   for (auto itr = begin; itr != end; ++itr) {
 
-      if (itr->status().type() == file_type::directory_file) {
+      if (itr->status().type() == file_type::directory_file
+              && !is_symlink(symlink_status(itr->path()))) {
           //recursive call
           process_directory(itr->path());
       }
-      else if (itr->status().type() == file_type::regular_file) {
+      else if (itr->status().type() == file_type::regular_file
+               && !is_symlink(symlink_status(itr->path()))) {
+         
           m_vector->emplace_back(itr->path(),
                                  file_size(itr->path()));
+      }
+      else if(is_symlink(symlink_status(itr->path()))){
+          //TODO
+          //option ignore symlink yes/no
       }
       else {
           std::cerr << "unexpected file type at: "<< itr->path() << "\n";
